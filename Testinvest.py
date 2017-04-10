c7a8d130691386956fe68895925b87234a17fb4d
Lukas Schneider <lukasschneider@casady.org>
2017-04-10 15:05:40 -0500

    Various experiments.

--- Testinvest.py
+++ Testinvest.py
@@ -1,11 +1,11 @@
 def comparePayoffs(amount, rate, monthly):
     month = 0
     year = 0
-    while amount > 0:
+    while amount >= 0:
         amount = amount - monthly
-        # print(amount)
+        print(amount)
         monthly = monthly * (rate / 100 / 12)
-        # print(monthly)
+        print(monthly)
         month = month + 1
         if month == 12:
             year = year + 1
@@ -13,4 +13,4 @@ def comparePayoffs(amount, rate, monthly):
     return (year, month)
 
 
-print(comparePayoffs(100, 200, 50))
+print(comparePayoffs(60000, 5, 500))
