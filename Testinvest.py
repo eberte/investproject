da40c8fe1713461684e436957ded1aa6281f59d3
Lukas Schneider <lukasschneider@casady.org>
2017-04-12 14:02:54 -0500

    imperfect printing statement

--- Testinvest.py
+++ Testinvest.py
@@ -16,8 +16,21 @@ def comparePayoffs(amount, rate, monthly1, monthly2):
         if month2 == 12:
             year2 = year2 + 1
             month2 = 0
+    if year1 > year2:
+        year3 = year1 - year2
+    else:
+        year3 = year2 - year1
+    if month1 > month2:
+        month3 = month1 - month2
+    else:
+        month3 = month2 - month1
+    if monthly1 > monthly2:
+        monthly3 = monthly1 - monthly2
+    else:
+        monthly3 = monthly2 - monthly1
     print('If you pay %g per month, the reypament period will be %g years and %g months.' % (monthly1, year1, month1))
     print('If you pay %g per month, the reypament period will be %g years and %g months.' % (monthly2, year2, month2))
+    print('If you pay %g more per month, the repayment period will be %g years and %g months shorter.' % (monthly3, year3, month3))
 
 
 comparePayoffs(60000, 5, 500, 750)
